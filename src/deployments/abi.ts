aa7d7c49f2418341c4dd4714109b2e9d52fd1e7a
Ephraim-nonso <chukwuchinonsoephraim@gmail.com>
2024-10-24 14:25:25 +0100

    "provider & org"

--- src/deployments/abi.ts
+++ src/deployments/abi.ts
@@ -644,6 +644,24 @@ export const attensysOrgAbi = [
   },
   {
     type: "struct",
+    name: "core::byte_array::ByteArray",
+    members: [
+      {
+        name: "data",
+        type: "core::array::Array::<core::bytes_31::bytes31>",
+      },
+      {
+        name: "pending_word",
+        type: "core::felt252",
+      },
+      {
+        name: "pending_word_len",
+        type: "core::integer::u32",
+      },
+    ],
+  },
+  {
+    type: "struct",
     name: "core::integer::u256",
     members: [
       {
@@ -716,7 +734,7 @@ export const attensysOrgAbi = [
       },
       {
         name: "org_name",
-        type: "core::felt252",
+        type: "core::byte_array::ByteArray",
       },
       {
         name: "number_of_instructors",
@@ -729,6 +747,10 @@ export const attensysOrgAbi = [
       {
         name: "number_of_all_classes",
         type: "core::integer::u256",
+      },
+      {
+        name: "nft_address",
+        type: "core::starknet::contract_address::ContractAddress",
       },
     ],
   },
@@ -752,7 +774,19 @@ export const attensysOrgAbi = [
     inputs: [
       {
         name: "org_name",
-        type: "core::felt252",
+        type: "core::byte_array::ByteArray",
+      },
+      {
+        name: "nft_name",
+        type: "core::byte_array::ByteArray",
+      },
+      {
+        name: "nft_symbol",
+        type: "core::byte_array::ByteArray",
+      },
+      {
+        name: "nft_uri",
+        type: "core::byte_array::ByteArray",
       },
     ],
     outputs: [],
@@ -980,6 +1014,10 @@ export const attensysOrgAbi = [
       {
         name: "owner",
         type: "core::starknet::contract_address::ContractAddress",
+      },
+      {
+        name: "class_hash",
+        type: "core::starknet::class_hash::ClassHash",
       },
     ],
   },
