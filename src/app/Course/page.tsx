8c7d5c29f7b201c6e8f9db04df45afb8f6798bfb
akinbola247 <akinbolakehinde247@gmail.com>
2024-11-01 17:03:24 +0100

    update to course route

--- src/app/Course/page.tsx
+++ src/app/Course/page.tsx
@@ -1,9 +1,18 @@
+"use client"
 import React from 'react'
+import Coursedropdown from '@/components/Coursedropdown'
+import { useAtom, useSetAtom } from "jotai"
+import { coursestatusAtom } from '@/state/connectedWalletStarknetkitNext'
+
+
 
 const index = () => {
+  const [status] = useAtom(coursestatusAtom); 
   return (
     <div>
-        this is courses page
+        {status && (<div className='fixed inset-0 bg-black opacity-5 backdrop-blur-sm'></div>)}
+        <Coursedropdown />
+        <h1>this is courses page</h1>
     </div>
   )
 }
