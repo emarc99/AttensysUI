99bf476949260e4b3e8a45fa2b5439a916a686df
akinbola247 <akinbolakehinde247@gmail.com>
2024-12-30 12:48:51 +0100

    added xplore & reg flow

--- src/components/bootcamp/Carosellcard.tsx
+++ src/components/bootcamp/Carosellcard.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import { Button } from '@headlessui/react'
+import { useRouter } from 'next/navigation';
 
 interface CarousellCardProp {
   name : string,
@@ -13,25 +14,58 @@ interface CarousellCardProp {
 }
 
 const Carosellcard : React.FC<CarousellCardProp> = (props) => {
-  return (
-    <div className={`relative ${`h-[${props.height}] w-[${props.width}]`} rounded-2xl mx-auto`}>
-    <Image src={props.flier} alt='eventimage' className='h-full w-full object-cover rounded-2xl' />
-    <Button className="hidden absolute top-3 right-6 justify-center lg:flex rounded-lg bg-[#9B51E0] text-[#FFFCFC] py-2 px-4 lg:h-[50px] items-center lg:w-[90px] text-sm data-[hover]:bg-sky-500 data-[active]:bg-sky-700">
-                          <div>{props.action}</div>
+  const router = useRouter();
 
-      </Button>
-    <div className='absolute bottom-0 z-20 w-full h-[150px] flex items-center justify-center text-center bg-carousell-gradient'>
-        <div className='flex space-x-3 mt-20'>
-        <div className='rounded-full h-[41px] w-[41px]'>
-        <Image src={props.logo} alt='logo' className=' object-cover' />
-        </div>
-        <div >
-        <h1 className='text-[#FFFFFF] text-[18px] font-bold leading-[22px]'>{props.name}</h1>
-        <h1 className='text-[#FFFFFF] text-[14px] font-medium leading-[13px]'>{props.time}</h1>
-        </div>
-        </div>
+   
+  const handleActionClick = (arg : any) => {
+    if (arg == "Register" ){
+    router.push(`/Register/${props.name}`)
+    }else if (arg == "Finished") {
+    router.push(`/Register/${props.name}`)
+    }
+  }
+  
+  return (
+    <div
+  className={`relative ${`h-[${props.height}] w-[${props.width}]`} rounded-2xl mx-auto overflow-hidden`}
+>
+  {/* Background Image */}
+  <Image
+    src={props.flier}
+    alt="eventimage"
+    className="h-full w-full object-cover"
+  />
+
+  {/* Action Button */}
+  <Button
+    onClick={() => handleActionClick(props.action)}
+    className="hidden absolute top-3 right-6 justify-center lg:flex rounded-lg bg-[#9B51E0] text-[#FFFCFC] py-2 px-4 lg:h-[50px] items-center lg:w-[90px] text-sm data-[hover]:bg-sky-500 data-[active]:bg-sky-700"
+  >
+    <div>{props.action}</div>
+  </Button>
+
+  {/* Bottom Section with Gradient */}
+  <div
+    className="absolute bottom-0 z-20 w-full h-[150px] flex items-center justify-center text-center bg-carousell-gradient rounded-b-2xl"
+  >
+    <div className="flex space-x-3 mt-20">
+      {/* Logo */}
+      <div className="rounded-full h-[41px] w-[41px] overflow-hidden">
+        <Image src={props.logo} alt="logo" className="object-cover" />
+      </div>
+      {/* Name and Time */}
+      <div>
+        <h1 className="text-[#FFFFFF] text-[18px] font-bold leading-[22px]">
+          {props.name}
+        </h1>
+        <h1 className="text-[#FFFFFF] text-[14px] font-medium leading-[13px]">
+          {props.time}
+        </h1>
+      </div>
     </div>
+  </div>
 </div>
+
   )
 }
 
