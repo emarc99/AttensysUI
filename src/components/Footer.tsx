2b5f42531db43fc68ffc5f76c6fed9d080b1ec92
Elite-tch <elite6461@gmail.com>
2025-01-23 17:21:48 +0100

    Your commit message describing the changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,12 +6,12 @@ import Logo from "@/assets/Logo.svg"
 
 const Footer = () => {
   return (
-    <div className='flex space-x-4 justify-between mx-4 py-7  w-[98%]'>
-            <div className='bg-[#161313] rounded-lg w-[40%] h-[380px] hidden lg:flex text-white justify-center pt-20'>
-                   <div className='h-[300px] space-y-2 lclg:w-[320px]'>
-                   <h1 className='font-semibold text-[34px]  leading-[40px] flex text-left w-[320px]'>Built by Geniuses, for Geniuses.</h1>
-                    <p className='font-normal text-[16px] leading-[23px] flex text-left w-[480px] pb-10 lclg:w-[320px]'>Join 1000+ designers, innovators, and creators who have embraced excellence, innovation, and creativity.</p>
-                    <Button className="hidden lg:flex rounded-lg bg-gradient-to-r from-[#9B51E0] to-[#4A90E2] py-2 px-4 lg:h-[56px] items-center lg:w-[154px] text-sm text-white data-[hover]:bg-sky-500 data-[active]:bg-sky-700">
+    <div className='flex lg:space-x-4 lg:flex-row mx-auto flex-col md:justify-between lg:mx-4 py-7 w-[90%]  lg:w-[98%] gap-6'>
+            <div className='bg-[#161313] rounded-lg w-[100%] md:w-[100%]  lg:w-[40%] h-auto md:h-[380px]   lg:flex text-white justify-center pt-10 md:pt-20'>
+                   <div className='md:h-[300px] h-[274.83px] space-y-2 lclg:w-[320px]'>
+                   <h1 className='font-semibold text-[34px] px-6 lg:px-0 leading-[40px] flex text-left md:w-[320px]'>Built by Geniuses, for Geniuses.</h1>
+                    <p className='font-normal text-[16px] leading-[23px] px-6 lg:px-0 flex text-left w-[95%] pb-10 lclg:w-[320px]'>Join 1000+ designers, innovators, and creators who have embraced excellence, innovation, and creativity.</p>
+                    <Button className=" lg:flex rounded-lg ml-6 lg:ml-0 bg-gradient-to-r from-[#9B51E0] to-[#4A90E2] py-2 px-4 lg:h-[56px] items-center lg:w-[154px] text-sm text-white data-[hover]:bg-sky-500 data-[active]:bg-sky-700">
                        <div className="flex space-x-2 items-center font-semibold text-[16px]">
                         <div>
                         Get Started             
@@ -26,30 +26,30 @@ const Footer = () => {
                    
             </div>
 
-            <div className='bg-[#F4E8FF] rounded-lg w-[58%] h-[380px] hidden lg:flex'>
-                        <div className='flex justify-between w-[80%] mx-auto py-10'>
-                                    <div className='flex space-x-28'>
-                                                <div className='space-y-10'>
-                                                    <h1 className='text-[19x] text-[#8D8484] leading-[18px] font-medium'>Product</h1>
-                                                    <div className='space-y-6 text-[13px] font-medium leading-[18px]'>
+            <div className='bg-[#F4E8FF] w-[100%] mx-auto rounded-lg lg:w-[80%] md:h-[380px]   flex flex-col'>
+                        <div className='flex md:justify-between w-[80%] mx-auto py-10 md:flex-row flex-col gap-6'>
+                                    <div className='flex md:space-x-28   flex-col md:flex-row'>
+                                                <div className='md:space-y-10 space-y-4'>
+                                                    <h1 className='text-[19px] text-[#8D8484] leading-[18px] font-medium'>Product</h1>
+                                                    <div className='md:space-y-6 space-y-4 text-[13px] font-medium leading-[18px]'>
                                                         <h1>Join Attensys</h1>
                                                         <h1>Verify certificates</h1>
                                                         <h1>Events</h1>
                                                         <h1>Certifcations</h1>
                                                     </div>
                                                 </div>
-                                                <div className='space-y-10'>
-                                                    <h1 className='text-[19x] text-[#8D8484] leading-[18px] font-medium'>Community</h1>
-                                                    <div className='space-y-6 text-[13px] font-medium leading-[18px]'>
+                                                <div className='md:space-y-10 space-y-4'>
+                                                    <h1 className='text-[19px] text-[#8D8484] leading-[18px] font-medium'>Community</h1>
+                                                    <div className='md:space-y-6 space-y-4 text-[13px] font-medium leading-[18px]'>
                                                         <h1>Attensys Community</h1>
                                                         <h1>Discord</h1>
                                                         <h1>Organizations</h1>
                                                         <h1>Get Support</h1>
                                                     </div>
                                                 </div>
-                                                <div className='space-y-10'>
+                                                <div className='md:space-y-10 space-y-4'>
                                                     <h1 className='text-[19x] text-[#8D8484] leading-[18px] font-medium'>Learn</h1>
-                                                    <div className='space-y-6 text-[13px] font-medium leading-[18px]'>
+                                                    <div className='md:space-y-6 space-y-4 text-[13px] font-medium leading-[18px]'>
                                                         <h1>Courses</h1>
                                                         <h1>Bootcamps</h1>
                                                         <h1>Registeration</h1>
@@ -64,8 +64,8 @@ const Footer = () => {
                                             />
                                  </div>
                                 
-                                <div className='flex justify-between w-[50%] mx-16 absolute mt-72 font-semibold text-[13px]'>
-                                        <div className='flex space-x-24'>
+                                <div className='flex justify-center md:justify-between items-center gap-3 md:w-[80%]  w-[90%] mx-auto  font-semibold text-[13px] pb-[5%] md:flex-row flex-col '>
+                                        <div className='flex space-x-28'>
                                             <h1>Privacy Policy</h1>
                                             <h1>Terms of Use</h1>
                                         </div>
