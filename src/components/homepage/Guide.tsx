1b4ae800bc17322e4a5ab00fbaf4d8f3a0a91ece
Jefferson <xjeffcmx23@gmail.com>
2025-01-25 19:00:19 -0600

    First changes

--- src/components/homepage/Guide.tsx
+++ src/components/homepage/Guide.tsx
@@ -1,72 +1,132 @@
-import React from "react"
-import thumbs from "@/assets/thumbs.svg"
-import Image from "next/image"
-import steps from "@/assets/steps.svg"
+import React from "react";
+import thumbs from "@/assets/thumbs.svg";
+import Image from "next/image";
+import steps from "@/assets/steps.svg";
 
 const Guide = () => {
   return (
     <div className="sm:h-[410px] w-[100%] py-12 sm:py-0 bg-[#2D3A4B] lg:flex">
+
+
       <div className="w-[90%] mx-auto h-[100%] my-auto block sm:flex sm:space-x-20 justify-center items-center">
-        <div className="flex items-center justify-around">
-          <div className="h-[299px] w-[347px] sm:order-1 order-last">
-            <Image alt="thumb" src={thumbs} className="" />
-          </div>
-          <div className="h-[276px] w-[43.96px] mx-0 sm:mx-16 order-2 sm:order-1">
-            <Image alt="steps" src={steps} className="" />
+        <div className="flex flex-col sm:flex-row items-center justify-around">
+          {/* Checks y Imagen */}
+          <div className="flex sm:space-x-8 items-center">
+            {/* Checks */}
+            <div
+              className="mx-0 sm:mx-20"
+              style={{
+                width: "39.35px",
+                height: "247px",
+              }}
+            >
+              <Image alt="steps" src={steps} className="mx-auto object-cover" />
+            </div>
+
+            {/* Imagen */}
+            <div
+              className="rounded-[8.26px] overflow-hidden"
+              style={{ width: "286.65px", height: "247px", marginLeft: "20px", marginTop: "auto" }}
+            >
+              <Image
+                alt="thumb"
+                src={thumbs}
+                className="object-cover"
+                width={286.65}
+                height={247}
+              />
+            </div>
           </div>
         </div>
 
-        <div className="space-y-6 w-[500px]">
-          <div className="space-y-2 px-4 py-6 sm:px-0 sm:py-0">
-            <h1 className="text-[18px] text-[#FFFFFF] font-bold leading-[21px]">
+        {/* Contenido de los textos */}
+        <div className="sm:space-y-8 space-y-6 w-[500px] mx-auto ">
+        <div className="text-[18px] sm:text-[18px] md:text-[20px] lg:text-[21px] space-y-4 px-4 py-6 sm:px-0 sm:py-0 ">
+            <h1 className="text-[#FFFFFF] font-bold leading-[21px]">
               Sign Up
             </h1>
-            <p className="text-[14.63px] text-[#FFFFFF] font-light text-justify">
+            <p
+              className="text-[14px] sm:text-[14px] leading-[20px] sm:leading-[24px] text-[#FFFFFF] font-light text-justify"
+              style={{ width: "100%", maxWidth: "332px", height: "auto", minHeight: "60px" }}
+            >
               Create an account and get instant access to our dashboard. Whether
               you are a school, organization, or event manager, we have the
               tools for you.
             </p>
           </div>
 
-          <div className="space-y-2 px-4 py-6 sm:px-0 sm:py-0">
-            <h1 className="text-[18px] text-[#FFFFFF] font-bold leading-[21px]">
+          <div className="text-[18px] sm:text-[18px] space-y-4 px-4 py-6 sm:px-0 sm:py-0">
+            <h1
+              className="text-[#FFFFFF] font-bold leading-[23px]"
+              style={{ width: "320px", height: "22px" }}
+            >
               Manage Attendance & Certifications
             </h1>
-            <p className="text-[14.63px] text-[#FFFFFF] font-light text-justify">
+            <p
+              className="text-[14px] text-[#FFFFFF] font-light text-justify"
+              style={{ width: "315px", height: "60px" }}
+            >
               Automatically track attendance and issue certificates upon course
               or event completion. Manage everything from one central platform.
             </p>
           </div>
         </div>
 
-        <div className="space-y-6 w-[500px]">
-          <div className="space-y-2 px-4 py-6 sm:px-0 sm:py-0">
-            <h1 className="text-[18px] text-[#FFFFFF] font-bold leading-[21px]">
+
+        {/* Div 2 */}
+        <div className="sm:space-y-8 space-y-6 w-[500px]">
+          <div className="text-[18px] sm:text-[18px] space-y-4 px-4 py-6 sm:px-0 sm:py-0">
+            <h1 className="text-[#FFFFFF] font-bold leading-[21px]">
               Set Up Courses & Events
             </h1>
-            <p className="text-[14.63px] text-[#FFFFFF] font-light text-justify">
-              Add your courses, events, or programs. Customize everything from
-              scheduling to certification rules.
+            <p
+              className="text-[14px] sm:text-[14px] leading-[20px] sm:leading-[24px] text-[#FFFFFF] font-light text-justify"
+              style={{ width: "100%", maxWidth: "332px", height: "auto", minHeight: "60px" }}
+            >
+              Add your courses, events, or programs.
+              Customize everything from scheduling
+              to certification rules
             </p>
           </div>
 
-          <div className="space-y-2 px-4 py-6 sm:px-0 sm:py-0">
-            <h1 className="text-[18px] text-[#FFFFFF] font-bold leading-[21px]">
+          <div className="text-[18px] sm:text-[18px] space-y-4 px-4 py-6 sm:px-0 sm:py-0">
+            <h1
+              className="text-[#FFFFFF] font-bold leading-[23px]"
+              style={{ width: "320px", height: "22px" }}
+            >
               Verify Certificate
             </h1>
-            <p className="text-[14.63px] text-[#FFFFFF] font-light text-justify">
-              You can now verify certification with as simple as a search button
-              click. Find out all you need with the{" "}
-              <span className="text-[#4A90E2] cursor-pointer">
+            <p
+              className="text-[14px] sm:text-[16px] leading-[20px] sm:leading-[24px] text-[#FFFFFF] font-light text-justify"
+              style={{
+                marginTop: "12px",
+                maxWidth: "350px",
+                lineHeight: "1.5",
+                padding: "5px 0",
+              }}
+            >
+              You can now verify certification with as simple as a search button click. Find out all you need with the{" "}
+              <span className="text-[#4A90E2] font-semibold cursor-pointer">
                 Attensys Explorer
               </span>
               .
             </p>
+
+
           </div>
         </div>
+
+
+
+
+
+
+
+
+
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Guide
+export default Guide;
--- src/components/homepage/Mantrasection.tsx
+++ src/components/homepage/Mantrasection.tsx
@@ -1,15 +1,15 @@
-import React from "react"
+import React from "react";
 
 const Mantrasection = () => {
   return (
-    <div className="mt-2 sm:h-[150px] w-[90%] mx-auto lg:flex justify-center sm:py-0 py-6">
-      <h1 className="text-[24px] font-bold sm:leading-[31px] w-[80%] mx-auto sm:flex justify-center align-middle">
+    <div className="mt-4 sm:mt-10 lg:mt-20 sm:h-[150px] w-[90%] mx-auto lg:flex justify-center sm:py-0 py-6">
+      <h1 className="hidden sm:block text-[24px] font-bold sm:leading-[31px] w-[80%] mx-auto sm:flex justify-center align-middle text-center">
         Powered by <span className="text-[#4A90E2]"> Starknet</span> — Ensuring
         Unmatched Security and Authenticity
         <br /> in course certification
       </h1>
     </div>
-  )
-}
+  );
+};
 
-export default Mantrasection
+export default Mantrasection;
