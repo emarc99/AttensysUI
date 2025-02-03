094ab1ec0a17d4d19574db8e752b72daaa2f9d59
akinbola247 <akinbolakehinde247@gmail.com>
2025-02-03 10:56:29 +0100

    header bug fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -112,7 +112,7 @@ const Header = () => {
     w-[98%] mx-auto 
      lclg:w-[100%] clg:w-[98%] xlg:w-[100%]`}
 >
-        <div className="lg:flex sm:hidden justify-center items-center sm:px-6 lg:px-8 lg:h-[85px] lg:my-auto clg:w-[100%] w-full sm1275:hidden">
+        <div className="lg:flex hidden sm:hidden justify-center items-center sm:px-6 lg:px-8 lg:h-[85px] lg:my-auto clg:w-[100%] w-full sm1275:hidden">
           <div className="relative flex h-20 items-center justify-between w-[98%]">
             <div className="lg:flex flex-shrink-0 items-center flex justify-between clg:w-[55%] lclg:w-[46%] lclg:mx-auto clg:mx-auto space-x-6 clg:space-x-6 lclg:space-x-6 md:hidden sm:hidden">
               <Link href="/" className="cursor-pointer">
